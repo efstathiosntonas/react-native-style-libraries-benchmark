77de81533919512f89f4f3d60b956c8d5c7fb39d
Efstathios Ntonas <stathis@hikiapp.com>
2023-07-26 10:00:58 +0300

    chore: add components folder on tailwind.config.js content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
