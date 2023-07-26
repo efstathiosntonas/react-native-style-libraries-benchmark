439d1f457b12fda4e66ff6b78c12173d26e5fc8d
Efstathios Ntonas <stathis@hikiapp.com>
2023-07-26 10:00:34 +0300

    chore: use NativeWind className instead of StyleSheet styles

--- components/NativeWind.js
+++ components/NativeWind.js
@@ -1,24 +1,16 @@
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { styled } from "nativewind";
 
-const StyledView = styled(View);
+const StyledView = styled(View, "border-2 p-1.5 border-red-600");
 
 const NativeWind = () => {
   return (
     <View style={{ display: "flex", flexDirection: "row" }}>
       {new Array(1000).fill(0).map((_, i) => (
-        <StyledView key={i} style={styles.styledView} />
+        <StyledView key={i} />
       ))}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  styledView: {
-    borderColor: "red",
-    borderWidth: 2,
-    padding: 5,
-  },
-});
 
 export default NativeWind;
