053f295584255ca39335c89ae7af365682db3968
Jacek Pudysz <jacekpudysz@gmail.com>
2023-10-03 14:20:04 +0200

    feat: add example with react-native-unistyles

---
+++ themes/unistyles-theme.js
@@ -0,0 +1,18 @@
+const palette = {
+  red: "red",
+}
+
+const theme = {
+  colors: {
+    red: palette.red,
+  },
+  spacing: {
+    xs: 5,
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
+  },
+};
+
+export default theme;
