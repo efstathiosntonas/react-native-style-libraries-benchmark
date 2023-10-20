48c6d590f1effe9ffcd419e09e7bede5d2e12875
Atanas Kanchev <atanas.kanchev@valens-services.co.uk>
2023-10-20 09:58:24 +0100

    chore: updated and pinned dep versions

--- App.js
+++ App.js
@@ -4,17 +4,15 @@ import { Button, StyleSheet, Text, View } from "react-native";
 
 import Dripsy from "./components/Dripsy";
 import EmotionNative from "./components/EmotionNative";
-import Gluestack from "./components/Gluestack";
+import FastStyles from "./components/FastStyles";
 import NativeWind from "./components/NativeWind";
 import Native from "./components/ReactNative";
 import Restyle from "./components/Restyle";
 import StyledComponents from "./components/StyledComponents";
-import Tamagui from "./components/Tamagui";
 import TimedRender from "./components/TimedRender";
 import Twrnc from "./components/Twrnc";
-import { Zephyr } from "./components/Zephyr";
-import FastStyles from "./components/FastStyles";
 import Unistyles from "./components/Unistyles";
+import { Zephyr } from "./components/Zephyr";
 
 export default function App() {
   const [styleType, setStyleType] = useState(undefined);
@@ -30,7 +28,7 @@ export default function App() {
       case "Styled Components":
         return <StyledComponents />;
       case "Tamagui":
-        return <Tamagui />;
+      // return <Tamagui />;
       case "Restyle":
         return <Restyle />;
       case "NativeWind":
@@ -42,7 +40,7 @@ export default function App() {
       case "Zephyr":
         return <Zephyr />;
       case "Gluestack":
-        return <Gluestack />;
+      // return <Gluestack />;
       case "Twrnc":
         return <Twrnc />;
       case "FastStyles":
