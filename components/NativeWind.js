310237bc2fb3986df8d29461eb7270c0c5911548
Divine Niiquaye Ibok <divineibok@gmail.com>
2024-10-14 12:29:09 +0000

    fixed nativewind benchmark (still not working)

--- components/NativeWind.js
+++ components/NativeWind.js
@@ -1,11 +1,13 @@
 import { View } from "react-native";
 import { COUNT } from "../utils";
 
+const StyledView = () => <View className="border-2 p-1.5 border-red-600" />;
+
 const NativeWind = () => {
   return (
     <View style={{ display: "flex", flexDirection: "row" }}>
       {new Array(COUNT).fill(0).map((_, i) => (
-        <View key={i} className="border-2 p-1.5 border-red-600" />
+        <StyledView key={i} />
       ))}
     </View>
   );
---
+++ metro.config.js
@@ -0,0 +1,26 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+
+/**
+ * @type {import('expo/metro-config')}
+ */
+const { getDefaultConfig } = require("@expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const path = require("path");
+
+// Find the project and workspace directories
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot, { isCSSEnabled: true });
+const globalCSS = path.resolve(projectRoot, "styles/global.css");
+const tailwindConfigPath = path.resolve(projectRoot, "tailwind.config.js");
+
+module.exports = withNativeWind(config, {
+    configPath: tailwindConfigPath,
+    input: globalCSS,
+    projectRoot,
+    inlineRem: false,
+    features: {
+        transformPercentagePolyfill: true,
+    }
+});
