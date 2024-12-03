529c0c8ee4256085f7407681f4727e11d825c13a
Divine Niiquaye Ibok <divineibok@gmail.com>
2024-12-03 06:53:38 +0000

    feat: remove gluestack

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,6 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
-      ["@gluestack-style/babel-plugin-styled-resolver"],
       [
         "@tamagui/babel-plugin",
         {
